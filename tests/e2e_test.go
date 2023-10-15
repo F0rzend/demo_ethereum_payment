644c38589b830b6ba499eb6318f543663ea4992f
F0rzend <zhogalkostya@gmail.com>
2023-10-16 01:17:09 +0300

    rename test helpers for http requests

--- tests/e2e_test.go
+++ tests/e2e_test.go
@@ -2,8 +2,12 @@ package tests
 
 import (
 	"context"
+	"fmt"
+	"net/http"
 	"testing"
 	"time"
+
+	"github.com/gavv/httpexpect"
 )
 
 func (s *TestSuite) TestApplication() {
@@ -11,24 +15,62 @@ func (s *TestSuite) TestApplication() {
 
 	const transactionValue = 1
 
-	invoiceID := createInvoice(s.T(), s.e(), transactionValue*2)
+	invoiceID := sendCreateInvoiceRequest(s.T(), s.e(), transactionValue*2)
 
-	invoice := getInvoice(s.T(), s.e(), invoiceID)
+	invoice := sendGetInvoiceRequest(s.T(), s.e(), invoiceID)
 	invoice.Status.Equal(InvoiceStatusPending)
 
 	tx := invoice.Deposit(s.eth, transactionValue)
 	tx.WaitConfirmation(ctx, s.eth)
 	waitForProcessing(s.T())
 
-	invoice = getInvoice(s.T(), s.e(), invoiceID)
+	invoice = sendGetInvoiceRequest(s.T(), s.e(), invoiceID)
 	invoice.Status.Equal(InvoiceStatusPending)
 
 	tx = invoice.Deposit(s.eth, transactionValue)
 	tx.WaitConfirmation(ctx, s.eth)
 	waitForProcessing(s.T())
 
-	invoice = getInvoice(s.T(), s.e(), invoiceID)
+	invoice = sendGetInvoiceRequest(s.T(), s.e(), invoiceID)
 	invoice.Status.Equal(InvoiceStatusPaid)
+}
+
+func sendCreateInvoiceRequest(t *testing.T, e *httpexpect.Expect, price uint64) uint64 {
+	t.Helper()
+
+	invoiceID := e.
+		POST("/invoices").
+		WithJSON(JSON{
+			"price": price,
+		}).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object().
+		Value("id").Number().Raw()
+
+	return uint64(invoiceID)
+}
+
+func sendGetInvoiceRequest(t *testing.T, e *httpexpect.Expect, invoiceID uint64) *TestInvoice {
+	t.Helper()
+
+	invoicePath := fmt.Sprintf("/invoices/%d", invoiceID)
+
+	invoice := e.
+		GET(invoicePath).
+		Expect().
+		Status(http.StatusOK).
+		JSON().Object()
+
+	return &TestInvoice{
+		t: t,
+
+		ID:      invoice.Value("id").Number(),
+		Price:   invoice.Value("price").Number(),
+		Balance: invoice.Value("balance").Number(),
+		Address: invoice.Value("address").String(),
+		Status:  invoice.Value("status").String(),
+	}
 }
 
 const transactionProcessingTime = 5 * time.Second
--- tests/test_invoice.go
+++ tests/test_invoice.go
@@ -2,9 +2,7 @@ package tests
 
 import (
 	"context"
-	"fmt"
 	"math/big"
-	"net/http"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -58,42 +56,4 @@ func (t *TestTransaction) WaitConfirmation(ctx context.Context, gateway *Ethereu
 	receipt, err := gateway.WaitForReceipt(ctx, t.transaction)
 	require.NoError(t.t, err)
 	require.NotNil(t.t, receipt)
-}
-
-func createInvoice(t *testing.T, e *httpexpect.Expect, price uint64) uint64 {
-	t.Helper()
-
-	invoiceID := e.
-		POST("/invoices").
-		WithJSON(JSON{
-			"price": price,
-		}).
-		Expect().
-		Status(http.StatusCreated).
-		JSON().Object().
-		Value("id").Number().Raw()
-
-	return uint64(invoiceID)
-}
-
-func getInvoice(t *testing.T, e *httpexpect.Expect, invoiceID uint64) *TestInvoice {
-	t.Helper()
-
-	invoicePath := fmt.Sprintf("/invoices/%d", invoiceID)
-
-	invoice := e.
-		GET(invoicePath).
-		Expect().
-		Status(http.StatusOK).
-		JSON().Object()
-
-	return &TestInvoice{
-		t: t,
-
-		ID:      invoice.Value("id").Number(),
-		Price:   invoice.Value("price").Number(),
-		Balance: invoice.Value("balance").Number(),
-		Address: invoice.Value("address").String(),
-		Status:  invoice.Value("status").String(),
-	}
 }
